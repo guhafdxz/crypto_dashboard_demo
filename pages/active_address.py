2c6a6ffc44d066e674c8dc56b9f80d8d67e6bf83
guhafdxz <08lujun@gmail.com>
2023-06-16 10:34:43 +0800

    Update active_address.py

--- pages/active_address.py
+++ pages/active_address.py
@@ -1,1 +1,12 @@
-ac
+import streamlit as st
+import pandas as pd
+
+
+st.set_page_config(
+    page_title='Active address demo',
+    page_icon="",
+)
+
+active_address=pd.read_excel('chain_daily_active_address.xlsx')
+
+st.line_chart(active_address)
