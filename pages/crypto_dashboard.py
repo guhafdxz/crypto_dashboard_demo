463d2b02035499ea1736592320ece7fbd2366f65
guhafdxz <08lujun@gmail.com>
2023-06-17 22:50:29 +0800

    Update crypto_dashboard.py

--- pages/crypto_dashboard.py
+++ pages/crypto_dashboard.py
@@ -19,6 +19,7 @@ upcoming_projects_df = pd.read_excel('./pages/Upcoming_ICO.xlsx')
 active_ico_projects_df =  pd.read_excel('./pages/Active_ICO.xlsx')
 latest_coin_events_df =pd.read_excel('./pages/coin_event_detail.xlsx')
 trending_coin_events_df=pd.read_excel('./pages/event_trending.xlsx')
+potential_coin_ranking =pd.read_excel('./pages/potential_coin.xlsx')
 fundraising_projects_df=pd.read_excel('./pages/fundraising_information.xlsx')
 st.title('This is a  :blue[crypto_dashboard] app demo :sunglasses:')
 tab1, tab2,tab3,tab4,tab5 = st.tabs(["Upcoming ICOs","Active ICOs", "Latest coin events","Trending coin events",'Fundrasing projects'])
@@ -39,6 +40,10 @@ with tab4:
     st.dataframe(trending_coin_events_df)
 
 with tab5:   
+    st.header('potential event-coins :red[ranking])
+    st.dataframe(potential_coin_ranking)
+              
+with tab6:   
     st.header(':green[latest] fundraising projects and details')
     st.dataframe(fundraising_projects_df)
 
